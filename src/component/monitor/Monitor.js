f040301dc9360a036e36d3cdd58fd0f00c75fbca
Shin Zo Long <chanin.master@gmail.com>
2021-03-22 16:30:18 +0700

    add edti delete

---
+++ src/component/monitor/Monitor.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import Calculator from './Calculator';
+import ProductList from '../product/ProductList';
+
+class Monitor extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {totalPrice: 0, orders: []};
+        this.addOrder = this.addOrder.bind(this);
+        this.delOrder = this.delOrder.bind(this);
+        this.cancelOrder = this.cancelOrder.bind(this);
+    }
+
+    addOrder(product){
+        let findOrder = this.state.orders.find(order => order.product.id === product.id);
+        if(findOrder){
+            findOrder.quantity++;
+        }else{
+            this.state.orders.push({product: product, quantity: 1});
+        }
+        const totalPrice = this.state.totalPrice + parseInt(product.unitPrice);
+        this.setState({totalPrice: totalPrice, orders: this.state.orders});
+    }
+
+    delOrder(product){
+        let findOrder = this.state.orders.find(order => order.product.id == product.id);
+        let resultOrder = this.state.orders.filter(order => order.product.id != product.id);
+        const totalPrice = this.state.totalPrice - (findOrder.quantity * parseInt(findOrder.product.unitPrice));
+        this.setState({totalPrice: totalPrice, orders: resultOrder});
+    }
+
+    cancelOrder(){
+        this.setState({totalPrice: 0, orders: []});
+    }
+
+    render(){
+        return(
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-9">
+                        <ProductList products={this.props.products} onAddOrder={this.addOrder}/>
+                    </div>
+                    <div className="col-md-3">
+                        <Calculator totalPrice={this.state.totalPrice} orders={this.state.orders} onDelOrder={this.delOrder} onCancelOrder={this.cancelOrder}/>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
+}
+export default Monitor;
--- src/component/product/ProductItem.js
+++ src/component/product/ProductItem.js
@@ -4,15 +4,24 @@ class ProductItem extends Component {
 
 constructor(props){
     super(props);
-    console.log('constructor |' + props.productName)
+    console.log('constructor |' + props.productName);
+}
+
+doSomething(productName){
+    console.log(productName)
 }
 
     render(){
-        const {productName, unitPrice} = this.props;
+        const {productName, unitPrice, thumbnail} = this.props.product;
         return(
-            <div>
-            <p>{productName}</p>
-            <p>{unitPrice}</p>
+            <div className="col-md-3 col-sm-6">
+            <img className="img-fluid img-thumbnail" src={thumbnail}  alt=""/>
+            <h5 className="mt-2">{productName}</h5>
+            <p className="title text-right">{unitPrice} THB</p>
+            <button className="btn btn-block btn-secondary title"  onClick={() => this.props.onAddOrder(this.props.product)}>
+                ซื้อ
+            </button>
+            <hr />
         </div>
         )
     }
